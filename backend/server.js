954412d6e222785a6b229e6a351b119d7cefcb3c
Marinos Veisllari <veisllarimarinos@gmail.com>
2025-10-14 14:08:04 +0300

    .........

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,22 @@ const allowedOrigins = [
   "chrome-extension://fhcbgnpgdmeckccdnhhnkpgdemiendbf",
 ];
 
+
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (origin && allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
   }
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  // Preflight OPTIONS request
   if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
+    return res.sendStatus(204); // No Content
   }
 
   next();
